13bd4132ab1fac9a184ab8899198e405a131f24d
mmarcott <mmarcott@student.42quebec.com>
2024-02-02 14:53:15 -0500

    fixing random generator for the robotomyRequestForm

--- ex02/RobotomyRequestForm.cpp
+++ ex02/RobotomyRequestForm.cpp
@@ -6,11 +6,13 @@
 /*   By: mmarcott <mmarcott@student.42quebec.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 13:06:39 by mmarcott          #+#    #+#             */
-/*   Updated: 2024/02/02 13:46:41 by mmarcott         ###   ########.fr       */
+/*   Updated: 2024/02/02 14:49:25 by mmarcott         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "RobotomyRequestForm.hpp"
+#include <cstdlib>
+#include <ctime>
 
 RobotomyRequestForm::RobotomyRequestForm(const std::string &target): AForm("Robotomy Request Form", 72, 45), _target(target) {}
 
@@ -32,14 +34,19 @@ const std::string	RobotomyRequestForm::getTarget(void) const {
 }
 
 void	RobotomyRequestForm::execute(const Bureaucrat &executor) const {
+	static time_t	lastTime = time(NULL);
+
 	if (!_signed)
 		throw SignedException();
 	if (executor.getGrade() > _executeRequiredGrade)
 		throw GradeTooLowException();
 
+	std::cout << "...Drilling noises..." << std::endl;
+	while (time(NULL) == lastTime)
+		srand(time(NULL));
 	int	randomNumber = std::rand();
+	lastTime = time(NULL);
 
-	std::cout << "...Drilling noises..." << randomNumber % 2 << std::endl;
 	if (randomNumber % 2 == 1)
 		std::cout << _target << " has been robotomized!" << std::endl;
 	else
